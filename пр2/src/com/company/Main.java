17522fc94887486ff649210c2c952b54c1874c3d
Fedry79 <101447648+Fedry79@users.noreply.github.com>
2022-03-12 10:11:59 +0300

    Add files via upload

---
+++ пр2/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+public class Main {
+        public static void main(String[] args) {
+
+                String str = "The quick brown fox Jumped over the lazy Dog";
+
+                System.out.println( Main.encode( str, 12 ));
+                System.out.println( Main.decode( Main.encode( str, 12), 12 ));
+        }
+
+        public static String decode(String enc, int offset) {
+                return encode(enc, 26-offset);
+        }
+
+        public static String encode(String enc, int offset) {
+                offset = offset % 26 + 26;
+                StringBuilder encoded = new StringBuilder();
+                for (char i : enc.toCharArray()) {
+                        if (Character.isLetter(i)) {
+                                if (Character.isUpperCase(i)) {
+                                        encoded.append((char) ('A' + (i - 'A' + offset) % 26 ));
+                                } else {
+                                        encoded.append((char) ('a' + (i - 'a' + offset) % 26 ));
+                                }
+                        } else {
+                                encoded.append(i);
+                        }
+                }
+                return encoded.toString();
+        }
+}
